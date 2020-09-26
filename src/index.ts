0e68c33939e3ab493e83ea42895f94df6e870d3c
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-09-25 18:30:13 -0700

    Auto scroll

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { compose } from './scene-exector';
 import { mainScene } from './main-scene';
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollToPlugin);
 
 compose(mainScene, document.getElementById('root')).execute();
+
+document.getElementById('play-button').addEventListener('click', () => {
+	gsap.to(window, {
+		scrollTo: {
+			y: 'max',
+			autoKill: true,
+		},
+		duration: 15,
+		ease: 'none',
+	})
+})
