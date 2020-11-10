50ab1eb0065ec6959a3a91642463cabad396c21b
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-11-10 13:26:59 -0800

    Add mobile styles for the intro text and remove play button

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import { compose } from './scene-exector';
 import { mainScene } from './main-scene';
 
-const { execute, scrollToBottom } = compose(mainScene, document.getElementById('root'));
-execute();
-
-document.getElementById('play-button').addEventListener('click', () => {
-	scrollToBottom();
-});
+compose(mainScene, document.getElementById('root')).execute();
